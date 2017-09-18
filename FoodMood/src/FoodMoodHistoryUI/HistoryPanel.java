b670b188b4bcdcafb092af2bf246e050af4233a3
Jake <Jake@192.168.1.9>
2017-09-17 22:00:57 -0400

    FoodMoodHistory proto

---
+++ FoodMood/src/FoodMoodHistoryUI/HistoryPanel.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FoodMoodHistoryUI;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Jake
+ */
+public final class HistoryPanel extends JPanel{
+    
+    /**
+     * Class Constructor. Initializes all UI components on the frame.
+     */
+    public HistoryPanel(){
+        initComponents();
+    }
+    
+    /**
+     * Initialize components method for the class. 
+     */
+    public void initComponents(){
+        
+    }
+    
+    
+}

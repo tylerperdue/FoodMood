8c00f8eb22e54c3605f6088dfb0be2f92e6d5e18
Jake <Jake@192.168.1.9>
2017-09-17 22:48:29 -0400

    recommendations

---
+++ FoodMood/src/RecommendationUI/RecommendationPanel.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RecommendationUI;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Jake
+ */
+public class RecommendationPanel extends JPanel{
+    
+    /**
+     * class constructor
+     */
+    public RecommendationPanel(){
+        
+        
+    }
+    
+    /**
+     * Initializes Components for the Panel.
+     */
+    public void initComponents(){
+        
+        
+    }
+}

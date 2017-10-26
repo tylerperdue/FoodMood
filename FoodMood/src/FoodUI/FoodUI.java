d6fe2510e470de2998d93e7a9bc9c78e1e1fa189
Bryan Ingram <bryan.ingram715@gmail.com>
2017-10-26 15:33:27 -0400

    Created FoodPanel, added it to FoodUI, and called after login

--- FoodMood/src/FoodUI/FoodUI.java
+++ FoodMood/src/FoodUI/FoodUI.java
@@ -18,12 +18,20 @@ public class FoodUI extends JFrame {
     
     public FoodUI(FoodController foodCntrl) {
         System.out.println("FoodUI Class Instantiated. Test Passed.");
-        FoodListPanel foodListPanel = new FoodListPanel(foodCntrl.viewFoodList(), foodCntrl);
-        CreateFoodPanel createFoodPanel = new CreateFoodPanel(foodCntrl);
+        //FoodListPanel foodListPanel = new FoodListPanel(foodCntrl.viewFoodList(), foodCntrl);
+        //CreateFoodPanel createFoodPanel = new CreateFoodPanel(foodCntrl);
+        FoodPanel foodPanel = new FoodPanel();
         this.setLayout(new BorderLayout());
+        this.setSize(500, 500);
+        this.setTitle("Food");
+        this.setResizable(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        this.add(foodPanel);
+        this.setVisible(true);   
         // TEST
-        this.add(foodListPanel);
-        this.add(createFoodPanel);
+        //this.add(foodListPanel);
+        //this.add(createFoodPanel);
         Food food = new Food("TEST", "This is a test food item.");
         foodCntrl.deleteFood(food);
         foodCntrl.updateFood(food);

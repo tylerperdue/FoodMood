1b809553bffa12f5c1270be24a6bbe29dcc9d24f
Tyler Perdue <perduetyler1@gmail.com>
2017-11-14 17:19:36 -0500

    sql file

---
+++ FoodMood/backup.sql
@@ -0,0 +1,11 @@
+CREATE TABLE APP.FOODMOODUSER (
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), 
+    username VARCHAR(25),
+    password VARCHAR(25),
+    first_name VARCHAR(25),
+    last_name VARCHAR(25),
+    age INTEGER,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO APP.FOODMOODUSER VALUES ("test", "test", "John", "Doe", 40);
